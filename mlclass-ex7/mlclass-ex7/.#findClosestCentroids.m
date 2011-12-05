37e00a827e92d7f8897e5ec4a19a7862734f2de2
Thom M <thom.mackey@gmail.com>
2011-12-05 22:20:57 +1100

    added ex7 homework to repo

---
+++ mlclass-ex7/mlclass-ex7/.#findClosestCentroids.m
@@ -0,0 +1,5 @@
+XSym
+0024
+7986d8e4e1c1f8a66201d71a551455b0
+mechamoth@Husk.local.502
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
