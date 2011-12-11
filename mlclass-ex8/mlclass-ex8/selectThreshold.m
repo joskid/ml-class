60843f84eb93c4f7ccd620492679f1b92fd0090b
tinkerthom <thom.mackey@gmail.com>
2011-12-11 15:48:36 +1100

    finished ex8 pts 1 & 2

--- mlclass-ex8/mlclass-ex8/selectThreshold.m
+++ mlclass-ex8/mlclass-ex8/selectThreshold.m
@@ -24,16 +24,17 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
+    predictions = pval < epsilon;
 
+    tp = sum((predictions == 1) & (yval == 1));
+    fp = sum((predictions == 1) & (yval == 0));
+    fn = sum((predictions == 0) & (yval == 1));
 
+    prec = tp / (tp + fp + 1e-100); # the +epsilon is to avoid div0 errors
+    rec = tp / (tp + fn);
 
-
-
-
-
-
-
-
+    F1 = (2 * prec * rec) / (prec + rec + 1e-100);
+    
 
     % =============================================================
 
